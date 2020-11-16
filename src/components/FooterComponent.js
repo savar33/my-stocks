cd5d443bec0a52d2d35fa1a8658b7588f1431c06
Misha Mas <savar33@gmail.com>
2020-11-16 17:20:35 +0200

    header and footer components2

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function FooterComponent(){
+    return (
+        <div className='footer'>
+            <footer className="py-5 bg-dark">
+            <div className="container">
+                <p className="m-0 text-center text-white">Michael Mas Copyright &copy; Website 2020</p>
+            </div>
+            </footer>
+        </div>
+    );
+}
+
+export default FooterComponent;
