e405fb8338fdd9f5705c10a3be077dfc9cac8fb4
Misha Mas <savar33@gmail.com>
2020-11-16 16:52:14 +0200

    move to components

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 
 function App() {
   return (
@@ -5,7 +6,7 @@ function App() {
       <div className='header'>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
           <div className="container">
-            <a className="navbar-brand" href="#">Stocks notebook</a>
+            <a className="navbar-brand" href="#">Stocks notebookkkkk</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
